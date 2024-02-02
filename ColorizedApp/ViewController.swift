1e3d9a58ff66c0a86b20c09a35933705f193789b
Alex Petrenko <alex.pt@ya.ru>
2024-02-02 19:41:44 +0300

    complete

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -1,19 +1,41 @@
-//
-//  ViewController.swift
-//  ColorizedApp
-//
-//  Created by Алексей on 02.02.2024.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var colorizedView: UIView!
+    
+    @IBOutlet weak var redValueLabel: UILabel!
+    @IBOutlet weak var greenValueLabel: UILabel!
+    @IBOutlet weak var blueValueLabel: UILabel!
+    
+    private var redColor: Float = 0
+    private var greenColor: Float = 0
+    private var blueColor: Float = 0
+    
+    @IBAction private func sliderActions(_ sender: UISlider) {
+        switch sender.tag {
+        case 0:
+            redValueLabel.text = String(round(100 * sender.value) / 100)
+            redColor = sender.value * 255
+            setupColorView(red: redColor, green: greenColor, blue: blueColor)
+        case 1:
+            greenValueLabel.text = String(round(100 * sender.value) / 100)
+            greenColor = sender.value * 255
+            setupColorView(red: redColor, green: greenColor, blue: blueColor)
+        default:
+            blueValueLabel.text = String(round(100 * sender.value) / 100)
+            blueColor = sender.value * 255
+            setupColorView(red: redColor, green: greenColor, blue: blueColor)
+        }
     }
-
-
+    
+    private func setupColorView(red: Float, green: Float, blue: Float) {
+        colorizedView.backgroundColor = UIColor(
+            red: CGFloat(red) / 255,
+            green: CGFloat(green) / 255,
+            blue: CGFloat(blue) / 255,
+            alpha: 1
+        )
+    }
 }
 
