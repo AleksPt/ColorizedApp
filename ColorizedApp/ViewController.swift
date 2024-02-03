ba9a211f8c693d94f301bbfd68bcad48dc35953e
Alex Petrenko <alex.pt@ya.ru>
2024-02-03 14:50:13 +0300

    refactoring

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -8,34 +8,62 @@ final class ViewController: UIViewController {
     @IBOutlet weak var greenValueLabel: UILabel!
     @IBOutlet weak var blueValueLabel: UILabel!
     
-    private var redColor: Float = 0
-    private var greenColor: Float = 0
-    private var blueColor: Float = 0
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    override func viewDidLoad() {
+        redSlider.value = Float.random(in: 0...1)
+        greenSlider.value = Float.random(in: 0...1)
+        blueSlider.value = Float.random(in: 0...1)
+        
+        setupTextLabel()
+        setupColorView(
+            red: redSlider.value,
+            green: greenSlider.value,
+            blue: blueSlider.value
+        )
+    }
     
     @IBAction private func sliderActions(_ sender: UISlider) {
         switch sender.tag {
         case 0:
-            redValueLabel.text = String(round(100 * sender.value) / 100)
-            redColor = sender.value * 255
-            setupColorView(red: redColor, green: greenColor, blue: blueColor)
+            setupTextLabel()
+            setupColorView(
+                red: redSlider.value,
+                green: greenSlider.value,
+                blue: blueSlider.value
+            )
         case 1:
-            greenValueLabel.text = String(round(100 * sender.value) / 100)
-            greenColor = sender.value * 255
-            setupColorView(red: redColor, green: greenColor, blue: blueColor)
+            setupTextLabel()
+            setupColorView(
+                red: redSlider.value,
+                green: greenSlider.value,
+                blue: blueSlider.value
+            )
         default:
-            blueValueLabel.text = String(round(100 * sender.value) / 100)
-            blueColor = sender.value * 255
-            setupColorView(red: redColor, green: greenColor, blue: blueColor)
+            setupTextLabel()
+            setupColorView(
+                red: redSlider.value,
+                green: greenSlider.value,
+                blue: blueSlider.value
+            )
         }
     }
     
     private func setupColorView(red: Float, green: Float, blue: Float) {
         colorizedView.backgroundColor = UIColor(
-            red: CGFloat(red) / 255,
-            green: CGFloat(green) / 255,
-            blue: CGFloat(blue) / 255,
+            red: CGFloat(red),
+            green: CGFloat(green),
+            blue: CGFloat(blue),
             alpha: 1
         )
+    }
+    
+    private func setupTextLabel() {
+        redValueLabel.text = String(format: "%.2f", redSlider.value)
+        greenValueLabel.text = String(format: "%.2f", greenSlider.value)
+        blueValueLabel.text = String(format: "%.2f", blueSlider.value)
     }
 }
 
